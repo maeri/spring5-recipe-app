86b318dddc18010a79374cfe8f47ee983c4f0c46
zawadma <marcin.zawadzki@ext.ec.europa.eu>
2021-01-03 22:32:54 +0100

    Complete @ManyToMany

---
+++ src/main/java/guru/springframework/domain/Difficulty.java
@@ -0,0 +1,9 @@
+package guru.springframework.domain;
+
+/**
+ * @author zawadma
+ * @date 03/01/2021 19:35
+ */
+public enum Difficulty {
+	EASY, MODERATE, HARD
+}
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -3,10 +3,15 @@ package guru.springframework.domain;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import lombok.Getter;
@@ -33,8 +38,17 @@ public class Recipe {
 	private String url;
 	private String directions;
 
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
+
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredients;
+
+	@ManyToMany
+	@JoinTable(name="recipe_category",
+			joinColumns = @JoinColumn(name = "recipe_id"),
+			inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories;
 
 	@Lob
 	private Byte[] image;
